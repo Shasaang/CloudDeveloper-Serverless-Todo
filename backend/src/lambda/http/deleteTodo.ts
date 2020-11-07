ecde2accf0af4328b9c0da223f5ad3f2c29f8f77
shasaang <shasaang29@gmail.com>
2020-11-07 22:57:00 +0530

    implementing token verifier

---
+++ backend/src/lambda/http/deleteTodo.ts
@@ -0,0 +1,27 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { deleteTodo } from '../../businessLayer/todos'
+import { createLogger } from '../../utils/logger'
+const logger = createLogger('auth')
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+
+    if (!(await deleteTodo(event))) {
+        logger.info("Item cannot be seen")
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          error: 'Item does not exist'
+        })
+      };
+    }
+  
+    return {
+      statusCode: 202,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({})
+    };
+  }
