df8506a4343d451697048b8f22f87a5ee13f13a8
Shasaang <53186606+Shasaang@users.noreply.github.com>
2020-11-07 23:50:07 +0530

    Updating config file

--- client/src/config.ts
+++ client/src/config.ts
@@ -2,7 +2,7 @@ const apiId = 'satf2j8t36'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: 'dev-05gs5dx8.us.auth0.com',            // Auth0 domain
-  clientId: 'CRC6kyv5GyyH0q1ELLUO6n364AOEZBh1',          // Auth0 client id
+  domain: 'dev-d7oyz4rj.us.auth0.com',            // Auth0 domain
+  clientId: 'v2x9iwBQTSCayCtLcLRFy2udW2hJ2rSP',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
